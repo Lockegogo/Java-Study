9c1388e5bb0f9c209f5861acd1aec8f7243fe015
Lockegogo <lmzhao31@outlook.com>
2024-02-16 11:34:17 +0800

    add @EnableDubbo

--- SpringBoot/springboot-09-dubbo_zookeeper/provider-server/src/main/java/com/locke/ProviderServerApplication.java
+++ SpringBoot/springboot-09-dubbo_zookeeper/provider-server/src/main/java/com/locke/ProviderServerApplication.java
@@ -1,9 +1,11 @@
 package com.locke;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo  // 服务提供方开启 Dubbo
 public class ProviderServerApplication {
 
     public static void main(String[] args) {
--- SpringBoot/springboot-09-dubbo_zookeeper/provider-server/src/main/java/com/locke/service/TicketServiceImpl.java
+++ SpringBoot/springboot-09-dubbo_zookeeper/provider-server/src/main/java/com/locke/service/TicketServiceImpl.java
@@ -1,14 +1,17 @@
 package com.locke.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Component;
 
 // 服务注册与发现
 @DubboService  // 可以被扫描到，在项目一启动就自动注册到注册中心 zookeeper
 @Component  // 使用 Dubbo 后尽量不要用 Service 注解
+@Slf4j
 public class TicketServiceImpl implements TicketService {
     @Override
     public String getTicket() {
+        log.info("买票服务被调用");
         return "《狂神说Java》";
     }
 }
